78a6b1044a8bb8ee171c338ad94bb5b37f4f5436
silverback23 <cahid23@hotmail.com>
2021-10-31 12:56:57 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace kararYapıları_SwitchCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Switch Case
+
+                int DateTi= DateTime.Now.Month;
+                switch(DateTi)
+              {              
+                case 1:
+                    Console.WriteLine("Ocak ayındasınız ");
+                    break;
+                case 2:
+                    Console.WriteLine("şubat ayındasınız ");
+                    break;
+                case 3:
+                    Console.WriteLine("mart ayındasınız ");
+                    break;
+                default:
+                    Console.WriteLine("Yanlış değer girdiniz ");
+                    break;
+                }
+        }
+    }
+}
